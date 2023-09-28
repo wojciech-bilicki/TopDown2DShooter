5fb3ec2721130c5156669d47a55e2399e02e5579
Wojciech Bilicki <wojciech.bilicki@gmail.com>
2023-09-28 21:44:30 +0200

    add ammo

--- Scenes/shooting_system.gd
+++ Scenes/shooting_system.gd
@@ -2,7 +2,16 @@ extends Node
 
 class_name ShootingSystem
 
+@export var total_ammo = 40 
+@export var magazine_size = 10
+var ammo_in_magazine = 0
+
+
+func _ready():
+	ammo_in_magazine = magazine_size
+	total_ammo -= ,magazine_size
 
 func _input(event):
 	if Input.is_action_just_pressed("shoot"):
 		print("Shoot")
+
